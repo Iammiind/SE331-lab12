17786616e233be5ca481c48c52e6cde6dd5033c8
Iammmiind <Onanong.pureemahawong@gmail.com>
2017-05-08 14:07:06 +0700

    Commit on choice 1.5

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const appRoutes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/list',
+    redirectTo: '/new-add',
     pathMatch: 'full'
   },
   {path: '**', component: FileNotFoundComponent},
